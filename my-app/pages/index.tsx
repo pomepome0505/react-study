affe900d6777191ea26b8aa05a0841d0db11f2c3
ryushi-masa <ohaoha.0610@gmail.com>
2024-06-05 23:01:48 +0900

    useEffectを使用して外部データをfetch

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -1,13 +1,6 @@
 import Head from "next/head";
+import { useState, useEffect } from 'react'
 const url = "http://localhost:3000/data.json"
-
-export async function getStaticProps(context) {
-  const result = await fetch(url)
-  const data = await result.json()
-  return {
-    props:{data:data}
-  }
-}
 
 type Props = {
   data: [
@@ -19,7 +12,25 @@ type Props = {
   ]
 }
 
+type Data = {
+  message: string
+  name: string
+  email: string
+}
+
 export default function Home(props: Props) {
+  const [data,setData] = useState(<tr><td>no data.</td></tr>)
+  useEffect(() => {
+    fetch(url).then(res => res.json()).then(res=>{
+      setData(res.map((value: Data) => 
+        <tr className="my-2 h6" key={value.name}>
+          <td>{value.message}</td>
+          <td>{value.name}</td>
+          <td>{value.email}</td>
+        </tr>
+      ))
+    })
+  },[])
   return (
     <div>
       <Head>
@@ -28,13 +39,16 @@ export default function Home(props: Props) {
       <main className="container">
         <h2>Index page.</h2>
         <div className="alert alert-primary my-3">
-          <ol>
-            {props.data.map(value=>
-              <li className="my-2 h6" key={value.name}>
-                {value.message}（{value.name}[{value.email}]）
-              </li>
-            )}
-          </ol>
+          <table className="alert alert-primary my-3">
+            <thead>
+              <tr>
+                <th>Message</th>
+                <th>Name</th>
+                <th>Email</th>
+              </tr>
+            </thead>
+            <tbody>{data}</tbody>
+          </table>
         </div>
       </main>
     </div>
