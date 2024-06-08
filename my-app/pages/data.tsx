1f018235088c3ff52225a7463072150f77a98aa0
ryushi-masa <ohaoha.0610@gmail.com>
2024-06-09 01:30:52 +0900

    動的ページの404対策

---
+++ my-app/pages/data.tsx
@@ -0,0 +1,9 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+
+export default function Data() {
+  const router = useRouter()
+  useEffect(() => {
+    router.replace('/data/0')
+  })
+}
