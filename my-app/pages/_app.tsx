5b78abae2533d093834cff4d73175f4c9665461b
ryushi-masa <ohaoha.0610@gmail.com>
2024-06-09 16:01:49 +0900

    プリレイアウト実装

--- my-app/pages/_app.tsx
+++ my-app/pages/_app.tsx
@@ -4,15 +4,20 @@ import Head from 'next/head'
 import Header from '../components/header'
 import Footer from '../components/footer'
 
-export default function App({ Component, pageProps }: AppProps) {
+type MyAppProps = AppProps & {
+  Component: AppProps['Component'] & { getPage?: (page: JSX.Element) => JSX.Element}
+}
+
+export default function App({ Component, pageProps }: MyAppProps) {
   pageProps.appname = "My Next App"
   pageProps.author = "SYODA-Tuyano"
-  return <div>
+  const getPage = Component.getPage || ((page) => page)
+  return getPage(<div>
     <Head>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"/>
     </Head>
     <Header appname={pageProps.appname} />
     <Component {...pageProps} />
     <Footer author={pageProps.author} />
-  </div>
+  </div>)
 }
--- my-app/pages/other.tsx
+++ my-app/pages/other.tsx
@@ -1,5 +1,3 @@
-import Link from 'next/link'
-
 type Props = {
 
 }
@@ -13,5 +11,26 @@ export default function Ohter(props: Props) {
       </div>
       <p></p>
     </main>
+  )
+}
+
+const modal = {
+  position:"fixed",
+  width:"50%",
+  left:"25%",
+  top:"25%",
+  padding:"20px"
+}
+
+Ohter.getPage = function(page) {
+  return (
+    <div>
+      <h6 className="text-danger text-center">*** development mode ***</h6>
+      {page}
+      <div className="text-danger text-center bg-white border border-5" style={modal}>
+        <h3>Caution!</h3>
+        <h5>これは、開発用のダミーページです。使用しないで下さい。</h5>
+      </div>
+    </div>
   )
 }
