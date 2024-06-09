45ee0d5e00c41bad895bb3788fd90a5f6be64219
ryushi-masa <ohaoha.0610@gmail.com>
2024-06-10 00:48:19 +0900

    APIを実装

--- my-app/pages/api/hello.ts
+++ my-app/pages/api/hello.ts
@@ -2,12 +2,28 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 
 type Data = {
-  name: string;
-};
+  id: number
+  data: dummyData
+}
+
+type  dummyData = {
+    name: string
+    email: string
+    tel: string
+}
+
+const data: dummyData[] = [
+  {name:'-', email:'-', tel:'-'},
+  {name:'Taro', email:'taro@yamada', tel:'090-999-999'},
+  {name:'Hanako', email:'hanako@flower', tel:'080-888-888'},
+  {name:'Sachiko', email:'sachiko@happy', tel:'070-777-777'}
+]
 
 export default function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>,
 ) {
-  res.status(200).json({ name: "John Doe" });
+  const id = req.query.id ? +req.query.id : 0
+  const result = data[id] ? data[id] : data[0]
+  res.status(200).json({id, data: result});
 }
